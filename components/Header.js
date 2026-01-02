05092bc49f2c98c23e7db1e00167d6804f7b842d
jonisrael <jonisrael45@gmail.com>
2026-01-02 15:18:59 -0600

    4

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,65 @@
 export default function Header({ title }) {
-  return <h1 className="title">"Header Here"</h1>;
+  return (
+    <header>
+      <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow mb-3">
+        <div class="container-fluid">
+          <a class="navbar-brand" asp-area="" asp-controller="Home" asp-action="Index">
+            How Unlucky!!!
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target=".navbar-collapse"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+            <ul class="navbar-nav flex-grow-1">
+              <li class="nav-item">
+                <a class="nav-link text-dark" asp-area="" asp-controller="Home" asp-action="Index">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link text-dark"
+                  asp-area=""
+                  asp-controller="Home"
+                  asp-action="SelectHand"
+                >
+                  Select Hand
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link text-dark"
+                  asp-area=""
+                  asp-controller="Home"
+                  asp-action="CreateNewHand"
+                >
+                  Create New Hand
+                </a>
+              </li>
+              @*{" "}
+              <li class="nav-item">
+                <a
+                  class="nav-link text-dark"
+                  asp-area=""
+                  asp-controller="Home"
+                  asp-action="CreateNewHand"
+                >
+                  About
+                </a>
+              </li>{" "}
+              *@
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
